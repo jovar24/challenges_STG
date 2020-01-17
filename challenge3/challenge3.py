cfea843e6945aae41adaea0968e9d8097433180b
Jose Vargas <jovar2497@gmail.com>
2020-01-16 20:06:39 -0700

    challenge2 and 3 half completed

---
+++ challenge3/challenge3.py
@@ -0,0 +1,34 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Challenge3(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def testchallenge3_for_loop(self):
+        self.driver.get("https://www.copart.com")
+        listofelements2 = self.driver.find_elements(By.XPATH, '//*[@ng-if="popularSearches"]//a')
+        for e in listofelements2:
+            print
+
+    def test_challenge3(self):
+        self.driver.get("https://www.copart.com")
+        listofelements = self.driver.find_elements(By.XPATH, '//*[@ng-if="popularSearches"]//a')
+        for e in listofelements:
+            print(e.text)
+
+        listofelements2= self.driver.find_elements(By.XPATH, '//*[@ng-if="popularSearches"]/../div[3]//a')
+        count = 0
+        while count < len(listofelements):
+            print(listofelements[count].text+":"+listofelements2[count].get_attribute("href"))
+            count += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
